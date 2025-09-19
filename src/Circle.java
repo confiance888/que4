548a629c319390323a9c9a43cfbaedef111c9532
confiance888 <umutoniconfiance8@gmail.com>
2025-09-19 08:10:37 +0200

    finding the Area of circle

---
+++ src/Circle.java
@@ -0,0 +1,42 @@
+class Circle extends Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public void displayShapeInfo() {
+        System.out.println("Shape: Circle");
+        System.out.println("Radius: " + radius);
+        System.out.println("Area: " + String.format("%.2f", calculateArea()));
+    }
+}
+
+// Rectangle subclass
+class Rectangle extends Shape {
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public double calculateArea() {
+        return length * width;
+    }
+
+    @Override
+    public void displayShapeInfo() {
+        System.out.println("Shape: Rectangle");
+        System.out.println("Length: " + length);
+        System.out.println("Width: " + width);
+        System.out.println("Area: " + String.format("%.2f", calculateArea()));
+    }
--- src/Shape.java
+++ src/Shape.java
@@ -2,4 +2,3 @@ abstract class Shape {
     public abstract double calculateArea();
     public abstract void displayShapeInfo();
 }
-
